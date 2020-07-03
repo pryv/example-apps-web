cbd4f7dcdc4eb98b6ab45d15b5f7f6b5d19f214a
Ilia Kebets <kebetsi@gmail.com>
2020-07-03 15:13:03 +0200

    move some code around

--- onboarding/script.js
+++ onboarding/script.js
@@ -11,47 +11,6 @@ let connection,
   apiEndpointDiv,
   apiEndpointText;
 
-const authSettings = {
-  spanButtonID: 'pryv-button', // span id the DOM that will be replaced by the Service specific button
-  onStateChange: pryvAuthStateChange, // event Listener for Authentication steps
-  authRequest: { // See: https://api.pryv.com/reference/#auth-request
-    requestingAppId: 'pryv-simple-form', // to customize for your own app
-    languageCode: 'en', // optional (default english)
-    requestedPermissions: [ 
-      {
-        streamId: 'body',
-        defaultName: 'Body',
-        level: 'manage' // permissions for the app to write data in stream 'Body'
-      },
-      {
-        streamId: 'baby',
-        defaultName: 'Baby',
-        level: 'manage' // permissions for the app to write data in stream 'Baby'
-      }
-    ],
-    clientData: {
-      'app-web-auth:description': {
-        'type': 'note/txt',
-        'content': 'This sample app demonstrates how an institution can track the weight of a newborn baby alongside with the mother\'s blood pressure to prevent hypertension.'
-      } // to customize according to your own use case
-    },
-  }
-};
-
-function pryvAuthStateChange(state) { // called each time the authentication state changes
-  console.log('##pryvAuthStateChange', state);
-  if (state.id === Pryv.Browser.AuthStates.AUTHORIZED) {
-    apiEndpointText.innerHTML = state.apiEndpoint;
-    apiEndpointDiv.style.visibility = 'visible';
-
-    // Normally you would create a connection object and do something with the data here.
-    //connection = new Pryv.Connection(state.apiEndpoint);
-  }
-  if (state.id === Pryv.Browser.AuthStates.INITIALIZED) {
-    connection = null;
-  }
-}
-
 window.onload = (event) => {
   serviceInfoSelect = document.getElementById('service-info-select');
   serviceInfoInput = document.getElementById('service-info-text');
@@ -67,8 +26,6 @@ window.onload = (event) => {
   serviceInfoSelect.addEventListener("change", setServiceInfo);
   document.getElementById('auth-request-button').addEventListener("click", authRequest);
   document.getElementById('fetch-service-info-button').addEventListener("click", fetchServiceInfo);
-
-  apiEndpointDiv.style.visibility = 'hidden';
 };
 
 function setServiceInfo() {
@@ -89,4 +46,40 @@ async function fetchServiceInfo() {
 
 async function authRequest() {
   Pryv.Browser.setupAuth(authSettings, serviceInfoInput.value);
+}
+
+const authSettings = {
+  spanButtonID: 'pryv-button', // span id the DOM that will be replaced by the Service specific button
+  onStateChange: pryvAuthStateChange, // event Listener for Authentication steps
+  authRequest: { // See: https://api.pryv.com/reference/#auth-request
+    requestingAppId: 'pryv-onboarding', // to customize for your own app
+    languageCode: 'en', // optional (default english)
+    requestedPermissions: [ 
+      {
+        streamId: 'health',
+        defaultName: 'Health',
+        level: 'manage' // permission for the app to manage data in the stream 'Health'
+      }
+    ],
+    clientData: {
+      'app-web-auth:description': {
+        'type': 'note/txt',
+        'content': 'This sample app demonstrates how an to implement an onboarding process for a Pryv.io platform.'
+      } // to customize according to your own use case
+    },
+  }
+};
+
+function pryvAuthStateChange(state) { // called each time the authentication state changes
+  console.log('##pryvAuthStateChange', state);
+  if (state.id === Pryv.Browser.AuthStates.AUTHORIZED) {
+    apiEndpointText.innerHTML = state.apiEndpoint;
+    apiEndpointDiv.style.visibility = 'visible';
+
+    // Normally you would create a connection object and do something with the data here.
+    //connection = new Pryv.Connection(state.apiEndpoint);
+  }
+  if (state.id === Pryv.Browser.AuthStates.INITIALIZED) {
+    connection = null;
+  }
 }
